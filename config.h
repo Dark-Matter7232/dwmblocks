a0104361e740e74a5d93e2a426a71900d8cce755
Dark-Matter7232 <kerneldeveloper7232@gmail.com>
2020-10-09 19:54:22 +0530

    Crime is common. Logic is rare.

--- config.h
+++ config.h
@@ -1,26 +1,18 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "cat /tmp/recordingicon 2>/dev/null",	0,	9},
-	/* {"",	"music",	0,	11},*/
-	{"",	"pacpackages",	0,	8},
-	{"",	"news",		0,	6},
-	/* {"",	"crypto",	0,	13}, */
-	/* {"",	"price bat \"Basic Attention Token\" 🦁",	0,	20}, */
-	/* {"",	"price btc Bitcoin 💰",				0,	21}, */
-	/* {"",	"price lbc \"LBRY Token\" 📚",			0,	22}, */
-	{"",	"torrent",	20,	7},
-	/* {"",	"memory",	10,	14}, */
-	/* {"",	"cpu",		10,	18}, */
-	/* {"",	"moonphase",	18000,	17}, */
-	{"",	"weather",	18000,	5},
-	{"",	"mailbox",	180,	12},
-	/* {"",	"nettraf",	1,	16}, */
-	{"",	"volume",	0,	10},
-	{"",	"battery",	5,	3},
-	{"",	"clock",	60,	1},
-	{"",	"internet",	5,	4},
-	{"",	"help-icon",	0,	15},
+                              /*Icon*/	               /*Command*/		                                               /*Update Interval*/	/*Update Signal*/
+		{"",       "cat /tmp/recordingicon 2>/dev/null",	        0,                  9     },
+		{"",	   "music",	                                        0,	            11    },
+		{"",	   "pacpackages",					0,	            8     },
+		{"|",	   "news",						0,	            6     },
+		{"|",	   "memory",						10,	            14    },
+		{"|",	   "mailbox",						180,	            12    },
+		{"|",	   "volume",						0,	            10    },
+		{"|",	   "clock",						60,	            1     },
+		{"|",	   "internet",						5,	            4     },
+		{"|",	   "dnd",						0,	            16    },
+		{"|",	   "help-icon",						0,	            15    },
+		{"|",	   "systray",						0,	            17    },
 };
 
 //Sets delimiter between status commands. NULL character ('\0') means no delimiter.
