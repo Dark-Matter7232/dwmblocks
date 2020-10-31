c41194092ff21907ae65abedbd2e38b0e50c4b3b
Dark-Matter7232 <kerneldeveloper7232@gmail.com>
2020-10-31 22:42:26 +0530

    Updates

--- config.h
+++ config.h
@@ -1,18 +1,18 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
-static const Block blocks[] = {
-                              /*Icon*/	               /*Command*/		                                               /*Update Interval*/	/*Update Signal*/
-		{"",       "cat /tmp/recordingicon 2>/dev/null",	        0,                  9     },
-		{"",	   "music",	                                        0,	            11    },
-		{"",	   "pacpackages",					0,	            8     },
-		{"|",	   "news",						0,	            6     },
-		{"|",	   "memory",						10,	            14    },
-		{"|",	   "mailbox",						180,	            12    },
-		{"|",	   "volume",						0,	            10    },
-		{"|",	   "clock",						60,	            1     },
-		// {"|",	   "internet",						5,	            4     },
-		{"|",	   "dnd",						0,	            16    },
-		{"|",	   "help-icon",						0,	            15    },
-		{"|",	   "systray",						0,	            17    },
+static Block blocks[] = {
+    /*Icon*/ /*Command*/ /*Update Interval*/ /*Update Signal*/
+					     //	{"",       "cat /tmp/recordingicon 2>/dev/null",	       0,                  9     },
+    {"󰄽 ", "songs", 2, 9},
+    {"󰄽 ", "pacpackages", 0, 8},
+    //	{"|",	   "news",						0,	            6     },
+    {"󰄽 ", "memory", 10, 14},
+    //	{"|",	   "mailbox",						180,	            12    },
+    {"󰄽 ", "volume", 0, 10},
+    {"󰄽 ", "clock", 60, 1},
+    // {"|",	   "internet",						5,	            4     },
+    {"󰄽 ", "dnd", 0, 16},
+    //	{"|",	   "help-icon",						0,	            15    },
+    {"󰄽", "", 0, 17},
 };
 
 //Sets delimiter between status commands. NULL character ('\0') means no delimiter.
