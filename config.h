375b531929a3aa3c1b2810da327e135ac18f91dc
Dark-Matter7232 <kerneldeveloper7232@gmail.com>
2020-11-02 13:54:32 +0530

    update

--- config.h
+++ config.h
@@ -2,11 +2,11 @@
 static Block blocks[] = {
     /*Icon*/ /*Command*/ /*Update Interval*/ /*Update Signal*/
 					     //	{"",       "cat /tmp/recordingicon 2>/dev/null",	       0,                  9     },
-    {"󰄽 ", "songs", 2, 9},
+    {"󰄽", "spot-sl", 2, 9},
     {"󰄽 ", "pacpackages", 0, 8},
     //	{"|",	   "news",						0,	            6     },
     {"󰄽 ", "memory", 10, 14},
-    //	{"|",	   "mailbox",						180,	            12    },
+    {"󰄽 ",	   "gmail",						15,	            12    },
     {"󰄽 ", "volume", 0, 10},
     {"󰄽 ", "clock", 60, 1},
     // {"|",	   "internet",						5,	            4     },
